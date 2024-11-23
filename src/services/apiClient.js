c5c2d908bd9ee02fe068cda335d35b1c3a4bfba7
Abhishek7145 <abhi27sahani@gmail.com>
2024-11-23 14:52:28 +0530

    updated cicd

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: "https://app.qa.techvgi.com/schoolerp-qa/api", // Replace with your API base URL
-  baseURL: "http://localhost:8181/api", // Replace with your API base URL
+  baseURL: "https://app.qa.techvgi.com/schoolerp-qa/api", // Replace with your API base URL
+  // baseURL: "http://localhost:8181/api", // Replace with your API base URL
   // baseURL: "http://localhost:8080/api", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json",
