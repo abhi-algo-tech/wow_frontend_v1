159fec58b5f7870c5ebea5fa832b9fd88b5d23dc
Abhishek7145 <abhi27sahani@gmail.com>
2024-12-08 04:03:07 +0530

    updated new ip

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://16.170.250.169:8080/api",
+  baseURL: "http://51.20.81.40:8080/api",
   // baseURL: "https://wow-backend-v1.onrender.com/api",
   // baseURL: "http://localhost:8080/api",
   timeout: 10000,
